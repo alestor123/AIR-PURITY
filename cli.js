1566deebc399066d13a9b9f1681678fdf3931b50
alestor123 <alestoraldous@gmail.com>
2021-01-18 14:32:27 +0530

    added main code

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var ap = require('./App'),
+labs = require('./labs.json');
+(async () => {
+var data = await ap(process.argv[2] || 1238);
+for (var [key, label] of Object.entries(labs)) console.log(`${label}: ${data.stats[key]}`);
+})();
